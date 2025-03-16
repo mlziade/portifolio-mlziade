063bf3d4db4880e68b087f6b9a4e9ea2c5fae7df
mlziade-elevential <ziade.michel@hotmail.com>
2025-03-16 11:22:58 -0300

    Add redirect from root URL to portfolio page

--- portifolio_mlziade/urls.py
+++ portifolio_mlziade/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portifolio/', include('portifolio.urls')),
+    path('', RedirectView.as_view(url='/portifolio/', permanent=True)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
