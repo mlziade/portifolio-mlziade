24724feb54fb63281c2011464f7d46fb56bb11e5
mlziade <ziade.michel@hotmail.com>
2025-06-08 14:35:23 -0300

    feat: Add robots.txt handling and caching to URL configuration

--- portifolio_mlziade/urls.py
+++ portifolio_mlziade/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.http import HttpResponse
+from django.views.decorators.cache import cache_control
+import os
 from portifolio.sitemaps import StaticViewSitemap, LanguageSpecificSitemap
 
 # Sitemap configuration
@@ -28,8 +31,19 @@ sitemaps = {
     'languages': LanguageSpecificSitemap,
 }
 
+def robots_txt(request):
+    """Serve robots.txt from static files"""
+    robots_path = os.path.join(settings.STATIC_ROOT or settings.STATICFILES_DIRS[0], 'robots.txt')
+    try:
+        with open(robots_path, 'r') as f:
+            content = f.read()
+        return HttpResponse(content, content_type='text/plain')
+    except FileNotFoundError:
+        return HttpResponse('User-agent: *\nDisallow:', content_type='text/plain')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robots.txt', cache_control(max_age=86400)(robots_txt), name='robots_txt'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', include('portifolio.urls')),
     path('playground/', include('playground.urls')),
