073dcc6c1011d42b5149d164d6bdb069e0e8f7fd
mlziade <ziade.michel@hotmail.com>
2025-11-09 00:23:40 -0300

    feat: register BlogPost, Tag, and Template models in admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import BlogPost, Tag, Template
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'published_date', 'created_at')
+    list_filter = ('status', 'published_date', 'tags')
+    search_fields = ('title', 'excerpt', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ('tags',)
+    date_hierarchy = 'published_date'
+    ordering = ('-published_date',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'created_at')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'file_path', 'is_default', 'created_at')
+    list_filter = ('is_default',)
+    search_fields = ('name', 'file_path', 'description')
